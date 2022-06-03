10c04ca5779424349d89bf9ce199efd4f78e25e7
Maurício Moreira-Soares <phydev@users.noreply.github.com>
2022-06-03 13:27:53 +0200

    create test.py

---
+++ tests/test.py
@@ -0,0 +1,9 @@
+from pyampute.ampute import MultivariateAmputation
+from mice import *
+
+ma = MultivariateAmputation(seed=42)
+X_amp = ma.fit_transform(df_np)
+
+df_amp = pd.DataFrame(X_amp, columns=df.columns, dtype=np.float64)
+
+imp = mice(X_amp, 20, 10, 42)
